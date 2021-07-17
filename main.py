891ebbeb83c0a3ad65cfbe3cef89e2b73d9408c4
Matt Bowring <mjb1085@wildcats.unh.edu>
2021-07-17 01:24:45 -0400

    more demos

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
 import random
+import numpy as np
+from math import pi, exp
 
 # function that adds two numbers
 function = lambda x, y: x + y
 print(function(2, 3))
-
 
 # function to create an empty graph network
 def create_graph_network(n):
@@ -13,7 +14,6 @@ def create_graph_network(n):
         graph[i] = []
 
     return graph
-
 
 # function to add an edge to the graph network (it actually generated this comment)
 def add_edge(graph, v1, v2):
@@ -42,5 +42,58 @@ def feature_embedding(graph, nodes):
             d[walk[i]] = i
         features[n] = d
     return features
+
+# create a Bayesian network (this is not quite right )
+def create_bayesian_network(n):
+    graph = create_graph_network(n)
+    for i in range(n):
+        for j in range(i+1, n):
+            add_edge(graph, i, j)
+    return graph
+
+# function to get the probability of a given path (this is also not quite right)
+def get_probability(path, features):
+    d = {}
+    for i in range(len(path)-1):
+        d[path[i]] = features[path[i+1]]
+    d[path[-1]] = features[path[0]]
+    return d
+
+# Wow Github Copilot, you are so smart!
+
+# function to get all factors of a number (used reduce?)
+def factors(n):
+    return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+# function to get the eigenvalues of a matrix without numpy 
+def eigenvalues_manual(matrix):
+    n = len(matrix)
+    eigvals = []
+    for i in range(n):
+        for j in range(i+1, n):
+            matrix[i][j] = matrix[j][i] = -matrix[i][j]
+    for i in range(n):
+        matrix[i][i] = 0
+        for j in range(n):
+            matrix[j][i] = 0
+    for i in range(n):
+        eigvals.append(sum(matrix[i]))
+    return eigvals
+
+# function to get eigvenvectors of a matrix with numpy
+def eigenvectors_manual(matrix):
+    n = len(matrix)
+    eigvals, eigvecs = np.linalg.eig(matrix) # lol, this was my first attempt at numpy
+    return eigvals, eigvecs
+
+# function to compute fft of signal 
+def fft(signal):
+    n = len(signal)
+    if n == 1:
+        return signal
+    even = fft(signal[0::2])
+    odd = fft(signal[1::2])
+    T = [exp(-2j*pi*k/n)*odd[k] for k in range(n//2)]
+    return [even[k] + T[k] for k in range(n//2)] + [even[k] - T[k] for k in range(n//2)]
 
 
